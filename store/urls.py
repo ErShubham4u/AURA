77be16e84fe41cb0f91e8d6bffb1bf0f7d386a52
ErShubham4u <connect2ershubham@gmail.com>
2026-09-05 15:31:26 +0530

    add cart quantity controls and remove item

--- store/urls.py
+++ store/urls.py
@@ -10,4 +10,21 @@ urlpatterns = [
     path("product/<slug:slug>/", views.product_detail, name="product_detail"),
     path("cart/", views.cart, name="cart"),
     path("cart/add/<int:product_id>/", views.add_to_cart, name="add_to_cart"),
+    path(
+    "cart/increase/<int:product_id>/",
+    views.increase_quantity,
+    name="increase_quantity"
+),
+
+path(
+    "cart/decrease/<int:product_id>/",
+    views.decrease_quantity,
+    name="decrease_quantity"
+),
+
+path(
+    "cart/remove/<int:product_id>/",
+    views.remove_from_cart,
+    name="remove_from_cart"
+),
 ]
